76d48dee0a624251fa437d427ada8e802e3de27d
daniel22haile <danimom9@gmail.com>
2022-09-17 21:05:19 -0500

    commit some changes...

--- dannyApp/src/app/app.component.ts
+++ dannyApp/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { LoginService } from './user-component/login/loginform.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-
+  constructor(public loginService: LoginService) {}
 }
